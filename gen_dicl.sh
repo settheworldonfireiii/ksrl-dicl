3c63b851e4398800a59ebadd4d8232dbe1251b1d
Marlin Lee <lee02328@ahl02.agate.msi.umn.edu>
2025-05-18 16:46:43 -0500

    more work interp current runs. Try to improve installing

--- gen_dicl.sh
+++ gen_dicl.sh
@@ -1,23 +1,46 @@
 #!/bin/bash -l
 #SBATCH --time=13:30:00
-#SBATCH --ntasks=4         # Increase number of tasks to match nproc_per_node
-#SBATCH --cpus-per-task=4  # Allocate 4 CPUs per task for data loading workers
+#SBATCH --ntasks=4
+#SBATCH --cpus-per-task=4
 #SBATCH --mem=50g
 #SBATCH --tmp=10g
 #SBATCH --mail-type=ALL
-#SBATCH --mail-user=radke149@umn.edu
+#SBATCH --mail-user=lee02328@umn.edu
 
+cd /scratch.global/lee02328/temp/ksrl-dicl
+#not done: SBATCH -p a100-4
+#not done: SBATCH --gres=gpu:a100:1
 
-cd /users/2/radke149/budgeting_vllm
+source /home/boleydl/lee02328/miniconda3/etc/profile.d/conda.sh
 
+# 1. Remove the environment if it exists, and proceed even if it doesn't (|| true)
+conda env remove -n dicl2 -y || true
 
-source /users/2/radke149/anaconda3/etc/profile.d/conda.sh
+conda clean --all -y || true
 
+pip cache purge || true
 
-conda activate /scratch.global/radke149/dicl-ksrl
+# 2. Create the environment, with -y to auto-confirm
+conda create -n dicl2 python=3.9 -y
 
+conda activate dicl2
 
-dicl-sac --seed 42 --env-id HalfCheetah-v4 --total-timesteps 100000 --exp_name "test_5p_dicl_s_pca_cl300__seed_42_shuffled" --batch_size 128 --llm_batch_size 7 --llm_learning_frequency 256 --context_length 300 --interact_every 1000 --learning_starts 5000 --llm_learning_starts 10000 --llm_model 'meta-llama/Llama-3.2-1B' --method 'dicl_s_pca'
+pip install .
+pip install .[rl]
 
+# 3. Install TensorFlow using pip, as the error originated from a pip-installed package
+pip install tensorflow
 
-
+dicl-sac --seed 42 \
+    --env-id HalfCheetah-v4 \
+    --total-timesteps 100000 \
+    --exp_name "temp_remove" \
+    --batch_size 128 \
+    --llm_batch_size 7 \
+    --llm_learning_frequency 256 \
+    --context_length 300 \
+    --interact_every 1000 \
+    --learning_starts 5000 \
+    --llm_learning_starts 10000 \
+    --llm_model 'meta-llama/Llama-3.2-1B' \
+    --method 'dicl_s_pca'
